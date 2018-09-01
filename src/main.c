40511592c083c344e8d8e540fec53524b38eee61
Rajib Singh <raj@rajibs-mbp.attlocal.net>
2018-09-01 07:37:34 -0400

    now closes when you his the escape key

--- src/main.c
+++ src/main.c
@@ -2,6 +2,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
 int main() {
     printf("Hello, Raj from /src!\n");
 
@@ -12,7 +18,7 @@ int main() {
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Shittiest Game in the World", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -21,9 +27,19 @@ int main() {
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glewExperimental = GL_TRUE;
+    glewInit();
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+
+    printf("%u\n", vertexBuffer);
+    glfwSetKeyCallback(window, key_callback);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
+//        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
