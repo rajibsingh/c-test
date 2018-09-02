1b2ab583c66e272c1c2783db6294ca78a21ed0ae
Rajib Singh <raj@rajibs-mbp.attlocal.net>
2018-09-02 15:50:53 -0400

    now close only when ESC is pressed

--- src/main.c
+++ src/main.c
@@ -4,8 +4,14 @@
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    if (action == GLFW_RELEASE) {
+        printf("%i\n", key);
+    }
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        printf("time to bail");
         glfwSetWindowShouldClose(window, GL_TRUE);
+    }
 }
 
 int main() {
