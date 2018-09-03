39ea32e38c5e4a3612c5e975d59f5931247e8d5e
Rajib Singh <raj@rajibs-mbp.attlocal.net>
2018-09-02 20:30:14 -0400

    drawing a white triangle woo!

--- src/main.c
+++ src/main.c
@@ -60,19 +60,57 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
-    
+    const char* vertex_shader =
+            "#version 400\n"
+            "in vec3 vp;"
+            "void main() {"
+            "  gl_Position = vec4(vp, 1.0);"
+            "}";
+
+    const char* fragment_shader =
+            "#version 400\n"
+            "out vec4 frag_colour;"
+            "void main() {"
+            "  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+            "}";
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
+
+    GLuint shader_programme = glCreateProgram();
+    glAttachShader(shader_programme, fs);
+    glAttachShader(shader_programme, vs);
+    glLinkProgram(shader_programme);
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
 //        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+//        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+//        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+//        glfwSwapBuffers(window);
 
         /* Poll for and process events */
+//        glfwPollEvents();
+
+        // wipe the drawing surface clear
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shader_programme);
+        glBindVertexArray(vao);
+        // draw points 0-3 from the currently bound VAO with current in-use shader
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // update other events like input handling
         glfwPollEvents();
+        // put the stuff we've been drawing onto the display
+        glfwSwapBuffers(window);
+
     }
 
     glfwTerminate();
